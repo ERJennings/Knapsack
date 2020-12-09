c5179242bdd165ac50acad526ae0b618268eec97
ERJennings <erjennings13@gmail.com>
2020-12-09 17:14:47 -0500

    Code seem sfine but getting errors

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class Knapsack {
+
+    public static int[] weights;
+    public static int[] values;
+    public static int[][] F;
+
     public static void main(String[] args) {
 
         //Get number of items
@@ -8,21 +13,45 @@ public class Knapsack {
         System.out.println("Enter the number of items: ");
         String enteredNumber = input.nextLine();
         int numItems = Integer.parseInt(enteredNumber);
+        weights = new int[numItems];
+        values = new int[numItems];
 
-        int[][] inputData = new int[numItems][2];
+        F = new int[numItems][numItems+1];
+        //Initialize all values to -1;
+        for (int i = 0; i < F.length; i++) {
+            for (int j = 0; j < F[0].length; i++) {
+                F[i][j] = 0;
+            }
+        }
 
         //Get weights
         for (int i = 0; i < numItems; i++) {
             System.out.println("What is the weight of item " + (i+1) + "? ");
             enteredNumber = input.nextLine();
-            inputData[i][0] = Integer.parseInt(enteredNumber);
+            weights[i] = Integer.parseInt(enteredNumber);
         }
 
         //Get values
         for (int i = 0; i < numItems; i++) {
             System.out.println("What is the value of item " + (i+1) + "? ");
             enteredNumber = input.nextLine();
-            inputData[i][1] = Integer.parseInt(enteredNumber);
+            values[i] = Integer.parseInt(enteredNumber);
         }
+
+        int maxValue = MFKnapsack(0,0);
+        System.out.print("The maximum possible vlue is " + maxValue);
+    }
+    public static int MFKnapsack(int i, int j) {
+        int value = -1;
+        if (F[i][j] < 0) {
+            if (j < weights[i]) {
+                value = MFKnapsack(i-1,j);
+            }
+            else {
+                value = Math.max(MFKnapsack(i-1,j), values[i] + MFKnapsack(i-1,j-weights[i]));
+            }
+        }
+        F[i][j] = value;
+        return F[i][j];
     }
 }
